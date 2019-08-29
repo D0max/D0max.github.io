975a179f781cf431e69cb85f21a64b1e2d0de480
Sobolev Maksim <sobolezm@gmail.com>
2019-08-29 16:31:35 +0600

    initial commit

---
+++ js/carouselConfig.js
@@ -0,0 +1,14 @@
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  dotsClass: true,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true
+
+});
